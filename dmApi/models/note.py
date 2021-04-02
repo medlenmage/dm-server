2f460ade2790398656cc9b3ab19ccad1e0302ef8
Joshua Medlen <joshua246@gmail.com>
2021-04-02 18:50:46 -0500

    created models for notes for campaigns. imported note into campaign model and imported campaign into dm model.

---
+++ dmApi/models/note.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+from django.db.models.deletion import DO_NOTHING
+
+class Note(models.Model):
+    note_title = models.TextField()
+    note_body = models.TextField()
+    note_date = models.DateTimeField()
